1b26e8865cd7f989a7fa1e8da5b1a9d8420598eb
João Pedro Mairinque <jpmairinque@gmail.com>
2021-06-20 18:09:17 -0300

    :heavy_check_mark: profile page done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -10,6 +10,9 @@ import {
 } from "./styles";
 
 const Profile = () => {
+
+
+  const [toggleEdit, setToggleEdit] = useState(false)
   const [profileData, setProfileData] = useState({
     name: "Karla Vaz",
     username: "thekarlavaz",
@@ -22,29 +25,35 @@ const Profile = () => {
 
       <PageHeader>
         <PageTitle>Profile</PageTitle>
-        <EditButton>A</EditButton>
+        <EditButton onClick={()=>{setToggleEdit(!toggleEdit)}}>{toggleEdit ? "Save" : "Edit Profile"}</EditButton>
       </PageHeader>
 
         <InputLabel>
-          <h3>NAME</h3>
-          <span>How should we call you?</span>
-          <h3>{profileData.name} </h3>
-          <Input type="text" />
+            <h3>NAME</h3>
+            <span>How should we call you?</span>
+            {toggleEdit ? <Input type="text" value={profileData.name} 
+            onChange={(e)=> setProfileData(prevState => ({...prevState, name:e.target.value}))}/> : <p>{profileData.name}</p>}
         </InputLabel>
+
         <InputLabel>
-          <h3>USERNAME</h3>
-          <span>It’s your ID on our plataform!</span>
-          <Input type="text" />
+            <h3>USERNAME</h3>
+            <span>It’s your ID on our plataform!</span>
+            {toggleEdit ? <Input type="text" value={profileData.username} 
+            onChange={(e)=> setProfileData(prevState => ({...prevState, username:e.target.value}))}/> : <p>{profileData.username}</p>}
         </InputLabel>
+
         <InputLabel>
-          <h3>EMAIL</h3>
-          <span>You can always change it.</span>
-          <Input type="text" />
+            <h3>EMAIL</h3>
+            <span>You can always change it.</span>
+            {toggleEdit ? <Input type="text" value={profileData.email} 
+            onChange={(e)=> setProfileData(prevState => ({...prevState, email:e.target.value}))}/> : <p>{profileData.email}</p>}
         </InputLabel>
+
         <InputLabel>
-          <h3>DATE OF BIRTH</h3>
-          <span>When were you born?</span>
-          <Input type="text" />
+            <h3>DATE OF BIRTH</h3>
+            <span>When were you born?</span>
+            {toggleEdit ? <Input type="text" value={profileData.birth} 
+            onChange={(e)=> setProfileData(prevState => ({...prevState, birth:e.target.value}))}/> : <p>{profileData.birth}</p>}
         </InputLabel>
         
     </PageContainer>
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -21,21 +21,42 @@ export const PageHeader = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 2rem;
+  padding: 2rem;
+  
+  button {
+    margin-right: 2rem;
+  }
 `;
 
 export const PageTitle = styled.h1`
-  padding: 2rem;
+
   font-size: 3rem;
   font-weight: 600;
 `;
 
 export const InputLabel = styled.div`
   margin-bottom: 2rem;
-
+  position: relative;
   display: flex;
   flex-direction: column;
   margin-left: 2rem;
 
+  &::before {
+    content: "";
+    background: #9b98ee;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 0.3rem;
+    transform: translate(-15px);
+  }
+
+  p {
+    margin-top: 0.2rem;
+    font-size: 1.2rem;
+  }
   h3 {
     color: #7d79ef;
   }
@@ -46,8 +67,18 @@ export const InputLabel = styled.div`
 `;
 
 export const EditButton = styled.button`
-  width: 1rem;
-  background-color: #9b98ee;
-  height: 30px;
-  border: none;
+  width: 100px;
+  height: 30;
+  background-color: transparent;
+  border: solid #9b98ee;
+  color: #9b98ee;
+  padding: 0.5rem;
+  border-radius: 50px;
+  cursor: pointer;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: #9b98ee;
+    color: white;
+  }
 `;
